0b1f4e4b5b408953cb8dad09ff3994dc88204399
Connor Stewart <27972245+ConnorStew@users.noreply.github.com>
2025-06-07 20:20:23 +0100

    Included patterns.

--- gpio.py
+++ gpio.py
@@ -4,34 +4,52 @@ import time
 GREEN_LED_PIN = 17
 RED_LED_PIN = 23
 BLUE_LED_PIN = 22
-SLEEP_TIME = 1
+SLEEP_TIME = 0.5
 
-def main():
-    pins = (GREEN_LED_PIN, RED_LED_PIN, BLUE_LED_PIN)
+pins = (GREEN_LED_PIN, RED_LED_PIN, BLUE_LED_PIN)
 
+def setup():
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
-
     for pin in pins:
         GPIO.setup(pin, GPIO.OUT)
 
+def cleanup():
+    GPIO.cleanup()
+
+# --- LED Patterns ---
+
+def pattern_all_on_then_off():
+    print("Pattern: All ON then OFF")
+    for pin in pins:
+        GPIO.output(pin, GPIO.HIGH)
+    time.sleep(SLEEP_TIME)
+    for pin in pins:
+        GPIO.output(pin, GPIO.LOW)
+    time.sleep(SLEEP_TIME)
+
+def pattern_blink_one_by_one():
+    print("Pattern: Blink One by One")
+    for pin in pins:
+        GPIO.output(pin, GPIO.HIGH)
+        time.sleep(SLEEP_TIME)
+        GPIO.output(pin, GPIO.LOW)
+
+
+def main():
+    setup()
     print(f"Running on pins: {pins}")
+
+    patterns = [
+        pattern_blink_one_by_one,
+    ]
 
     try:
         while True:
-            time.sleep(SLEEP_TIME)
-            for pin in pins:
-                print(f"Pin: {pin} on.")
-                GPIO.output(pin, GPIO.HIGH)
-
-            print(f"Sleeping: {SLEEP_TIME}")
-            time.sleep(SLEEP_TIME)
-            for pin in pins:
-                print(f"Pin: {pin} off.")
-                GPIO.output(pin, GPIO.LOW)
-            print(f"Sleeping: {SLEEP_TIME}")
+            for pattern in patterns:
+                pattern()
     except KeyboardInterrupt:
         print("Exiting gracefully")
-        GPIO.cleanup()
+        cleanup()
 
 main()
