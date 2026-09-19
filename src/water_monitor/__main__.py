e8f4d692acace7baca5f54a658f556f929bbbbc9
Connor Stewart <27972245+ConnorStew@users.noreply.github.com>
2026-09-19 20:11:02 +0100

    Moved main code into __main__

--- src/water_monitor/__main__.py
+++ src/water_monitor/__main__.py
@@ -1,4 +1,41 @@
-from water_monitor.main import main
+import time
+
+from water_monitor.gpio import GPIO
+from water_monitor.monitor import WaterMonitor
+from water_monitor.water_level import WaterLevel
+from water_monitor.audio import Audio
+from water_monitor.logger import logger
+
+def main() -> None:
+    logger.debug("Starting monitoring...")
+
+    gpio = GPIO()
+    water_monitor = WaterMonitor()
+    audio = Audio()
+
+    audio.play_random_welcome()
+
+    previous_level = None
+
+    while True:
+        freq = gpio.measure_frequency()
+        logger.debug(f"Measured Frequency: {freq:.1f} Hz")
+
+        level = water_monitor.map_frequency_to_level(freq)
+        logger.info(f"Matched level: {level}")
+
+        gpio.show_level(level)
+
+        if level != previous_level:
+            if level == WaterLevel.WITHOUT_LIQUID:
+                audio.play_random_dry()
+            elif level != WaterLevel.UNKNOWN:
+                audio.play_random_watered()
+            previous_level = level
+
+        audio.tick()
+
+        time.sleep(0.2)
 
 if __name__ == "__main__":
     main()
